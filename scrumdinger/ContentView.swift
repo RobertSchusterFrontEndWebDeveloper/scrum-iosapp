2d9173f60dd3c39169fc296cf408346db236ec14
Rob Schuster <robert.is@icloud.com>
2021-09-11 09:46:40 -0400

    Initial Commit

---
+++ scrumdinger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scrumdinger
+//
+//  Created by Robert Schuster on 9/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Robert Schuster on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
