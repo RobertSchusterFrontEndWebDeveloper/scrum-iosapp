82f83ef8898a0502c42a497e86afbf5bdcbb2287
Rob Schuster <robert.is@icloud.com>
2021-09-11 23:04:44 -0400

    Created Data Updater Model, user can edit all properties, details, color picker, slider, pick attendees, add new attendees, @State properties

--- scrumdinger/DetailView.swift
+++ scrumdinger/DetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
-    
+    @State private var isPresented = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -42,7 +42,21 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+            EditView()
+                .navigationTitle(scrum.title)
+                .navigationBarItems(leading: Button("Cancel") {
+                    isPresented = false
+                }, trailing: Button("Done") {
+                    isPresented = false
+                })
+            }
+        }
     }
 }
 
